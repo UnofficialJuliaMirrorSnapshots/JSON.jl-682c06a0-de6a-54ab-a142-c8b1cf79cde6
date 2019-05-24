41bfdcd8f65a3359fae79d0d68680f033033329f
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-24 11:07:14 +0000

    Snapshot of branch master taken on 2019-05-24T07:07:12.913-04:00 from "https://github.com/UnofficialJuliaMirror/JSON.jl-682c06a0-de6a-54ab-a142-c8b1cf79cde6"

--- src/Writer.jl
+++ src/Writer.jl
@@ -351,6 +351,17 @@ print(io::IO, obj) = show_json(io, StandardSerialization(), obj)
 print(a, indent) = print(stdout, a, indent)
 print(a) = print(stdout, a)
 
+"""
+    json(a)
+    json(a, indent::Int)
+
+Creates a JSON string from a Julia object or value.
+
+Arguments:
+  • a: the Julia object or value to encode
+  • indent (optional number): if provided, pretty-print array and object
+    substructures by indenting with the provided number of spaces
+"""
 json(a) = sprint(print, a)
 json(a, indent) = sprint(print, a, indent)
 
